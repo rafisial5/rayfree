3c7548e2ee540f5e8328056700b8fd0cfa1841eb
SyedMehtab <syedmehtab2125@gmail.com>
2023-10-14 22:12:39 +0500

    route changed for the user routing

--- lib/controller/select_car_or_toll_controller.dart
+++ lib/controller/select_car_or_toll_controller.dart
@@ -1,26 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:rayfree_toll/utils/constants.dart';
 
-class SelectCarOrTollController extends GetxController{
-
+class SelectCarOrTollController extends GetxController {
   void moveToCarOwnerLoginScreen() async {
-    if (kDebugMode) {
-      print("======> before method call");
-    }
-    incrementCounter();
-    print("======> after method call");
+    // if (kDebugMode) {
+    //   print("======> before method call");
+    // }
+    // incrementCounter();
+    // print("======> after method call");
 
-  // Get.toNamed(kCarOwnerLoginScreenRoute);
+    Get.toNamed(kCarOwnerLoginScreenRoute);
   }
+
   void incrementCounter() async {
     print("======> inside method call");
 
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: "mickellljcjd@gmail.com",
-        password: "123456").then((value) async {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+            email: "mickellljcjd@gmail.com", password: "123456")
+        .then((value) async {
       print("======> Email registered");
 
       await FirebaseFirestore.instance.collection('users').add({
@@ -30,9 +30,9 @@ class SelectCarOrTollController extends GetxController{
       print("=====> value: ${value.toString()}");
       print("=======> okay");
     });
-
   }
-  void moveToTollServiceLoginScreen(){
+
+  void moveToTollServiceLoginScreen() {
     Get.toNamed(kTollDashboardScreenRoute);
   }
 }
